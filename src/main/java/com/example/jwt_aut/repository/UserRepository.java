87660f3242f626d3e3f38898481d458f58f4e0d8
Dimas Rocha <dimas.dasp@gmail.com>
2025-11-02 19:23:23 -0300

    UserRepository

---
+++ src/main/java/com/example/jwt_aut/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.jwt_aut.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.jwt_aut.entity.User;
+
+@Repository
+public interface UserRepository {
+	Optional<User> findByUsername(String username);
+	boolean existsByUsername(String username);
+	@Query("SELECT u FROM User u WHERE u.role = :role")
+	Optional<User> findByRole(String role);
+
+}
