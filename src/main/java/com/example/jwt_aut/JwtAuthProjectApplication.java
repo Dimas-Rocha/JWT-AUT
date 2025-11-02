5eadef9610a2316ef3c68570674783a56b1275ee
Dimas Rocha <dimas.dasp@gmail.com>
2025-11-02 18:28:27 -0300

    Project init

---
+++ src/main/java/com/example/jwt_aut/JwtAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_aut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_aut/JwtAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_aut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
